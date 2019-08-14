6d88cc522ef3eb205606c5e8d699a71f5de1bca3
sshilpi <shilpi.shilpi@soprasteria.com>
2019-08-14 14:04:03 +0530

    Spring security using UserDetailService Implementation

---
+++ spring-rest-security-javaconfig/src/main/java/com/concretepage/dao/UserInfoDAO.java
@@ -0,0 +1,26 @@
+package com.concretepage.dao;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.hibernate5.HibernateTemplate;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.concretepage.entity.UserInfo;
+@Repository
+@Transactional
+public class UserInfoDAO implements IUserInfoDAO {
+	@Autowired
+	private HibernateTemplate hibernateTemplate;
+	@Override
+	public UserInfo getActiveUser(String userName) {
+		UserInfo activeUserInfo = new UserInfo();
+		short enabled = 1;
+		List<?> list = hibernateTemplate.find("FROM UserInfo WHERE userName=? and enabled=?",
+				userName, enabled);
+		if(!list.isEmpty()) {
+			activeUserInfo = (UserInfo)list.get(0);
+		}
+		return activeUserInfo;
+	}
+}
